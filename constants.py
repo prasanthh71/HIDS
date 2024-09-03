878d77580130158fc2bb12ae15354a13869586d3
seabeePraveen <cbsrirangapraveen.2004@gmail.com>
2024-09-03 15:51:52 +0530

    added .venv in gitignore and r to the strings in monitoring files in constants

--- constants.py
+++ constants.py
@@ -3,25 +3,25 @@ parsed_rules_file = './parsedRules.json'
 test_directory = './tests'
 
 monitoring_files = {
-    'windows':[
-        "C:\Windows\System32\config\SAM",
-        "C:\Windows\System32\config\SYSTEM",
+    'windows': [
+        r"C:\Windows\System32\config\SAM",
+        r"C:\Windows\System32\config\SYSTEM",
         # Hosts file for network configurations.
-        "C:\Windows\System32\drivers\etc\hosts",
-        "C:\Windows\System32\winevt\Logs\Security.evtx",
-        "C:\Windows\System32\winevt\Logs\Application.evtx",
-        "C:\Windows\System32\winevt\Logs\System.evtx",
-        "C:\Windows\explorer.exe",
-        "C:\Windows\System32\winlogon.exe",
-        "C:\Windows\System32\services.exe",
-        "C:\Windows\System32\svchost.exe",
-        "C:\Windows\System32\ntoskrnl.exe",
-        "C:\Windows\System32\kernel32.dll",
-        "C:\Windows\System32\user32.dll",
-        "C:\Windows\System32\firewall.cpl",
-        "C:\Program Files\Common Files\System\masadc"
-        ],
-    'linux':[
+        r"C:\Windows\System32\drivers\etc\hosts",
+        r"C:\Windows\System32\winevt\Logs\Security.evtx",
+        r"C:\Windows\System32\winevt\Logs\Application.evtx",
+        r"C:\Windows\System32\winevt\Logs\System.evtx",
+        r"C:\Windows\explorer.exe",
+        r"C:\Windows\System32\winlogon.exe",
+        r"C:\Windows\System32\services.exe",
+        r"C:\Windows\System32\svchost.exe",
+        r"C:\Windows\System32\ntoskrnl.exe",
+        r"C:\Windows\System32\kernel32.dll",
+        r"C:\Windows\System32\user32.dll",
+        r"C:\Windows\System32\firewall.cpl",
+        r"C:\Program Files\Common Files\System\masadc"
+    ],
+    'linux': [
         # User account information and passwords.
         "/etc/passwd",
         "/etc/shadow",
